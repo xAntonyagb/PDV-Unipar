a4403c38c98efdc40aa2784647f050027810be9d
Gabriel Garcia Couto <127454621+gabrielgc2003unipar@users.noreply.github.com>
2024-05-28 20:27:58 -0300

    Classe de Entidade da Venda

--- pdv-api/src/main/java/org/example/pdvapi/entities/Venda.java
+++ pdv-api/src/main/java/org/example/pdvapi/entities/Venda.java
@@ -2,6 +2,8 @@ package org.example.pdvapi.entities;
 
 import jakarta.persistence.*;
 
+import java.sql.Timestamp;
+
 @Entity
 @Table(name = "venda")
 public class Venda {
@@ -9,9 +11,59 @@ public class Venda {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String observacao;
-    private String data;
+    private Timestamp data;
     private double valorTotal;
     @ManyToOne
     private Cliente cliente;
 
+    public Venda() {
+    }
+
+    public Venda(int id, String observacao, Timestamp data, double valorTotal, Cliente cliente) {
+        this.id = id;
+        this.observacao = observacao;
+        this.data = data;
+        this.valorTotal = valorTotal;
+        this.cliente = cliente;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public Timestamp getData() {
+        return data;
+    }
+
+    public void setData(Timestamp data) {
+        this.data = data;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 }
