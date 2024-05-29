889b7509993d7e432ce1292a3736947a2e36802b
WallysonVict0r <wallyson.correa@edu.unipar.br>
2024-05-28 22:15:18 -0300

    commit controller produto + cliente

---
+++ pdv-api/src/main/java/org/example/pdvapi/controllers/ProdutoController.java
@@ -0,0 +1,75 @@
+package org.example.pdvapi.controllers;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.example.pdvapi.Services.ProdutoService;
+import org.example.pdvapi.entities.Cliente;
+import org.example.pdvapi.entities.Produto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+    @Autowired
+    private ProdutoService produtoService;
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok", content =
+                    { @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = Cliente.class)) }),
+            @ApiResponse(responseCode = "400", description = "ID invalido informado"),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content =
+                    { @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = ApiException.class)) }) })
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(produtoService.getById(id));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Produto>> getAll() {
+        return ResponseEntity.ok(produtoService.getAll());
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> insert(@RequestBody @Valid Produto produto,
+                                          UriComponentsBuilder builder) {
+
+        produtoService.insert(produto);
+
+        URI uri =
+                builder.path("/produto/{id}").
+                        buildAndExpand(produto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(produto);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> update(@PathVariable int id,
+                                          @RequestBody Produto produto) {
+        produtoService.update(produto);
+
+        return ResponseEntity.ok(produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        produtoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
